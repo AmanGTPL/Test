27aac69a1cc881ae051f12fb9306f5c8aaf35d94
Naman Sharma <naman.sharma@gaurish.com>
2025-03-24 15:39:46 +0530

    first commit - aman

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+	//$color = "red";
+	//echo "My car color is ".$color;
+	?>
+	<br>
+	<?php
+	//echo "My beg color is ".$color;
+	?>
+
+	<!-- -------------------------------------- -->
+
+	<?php
+	// $x = 8;
+	// $y = 5;
+	// echo $x*$y;
+	?>
+
+	<!-- -------------------------------------------------- -->
+	 <?php
+	//  $x = 12;
+	//  var_dump($x);
+
+	//  $y = "Projet";
+	//  var_dump($y);
+
+	//  $z = true;
+	//  var_dump($z);
+
+	//  $xx = 3.1434343;
+	//  var_dump($xx);
+	
+	//  $xy = [1,2,3,5];
+	//  var_dump($xy);
+
+	//  $xz = NULL;
+	//  var_dump($xz);
+	 ?>
+	<!-- -------------------------------------------------------------  -->
+	 <?php
+	//  $x = 8;
+	//  function func(){
+	// 	echo "<p>Number of pages. $x</p>";
+	//  }
+	//  func();
+	//  echo "Number of Pages ".$x;
+	 ?>
+	 <!-- ---------------------------------------------------------------- -->
+
+	 <?php
+	//  function func(){
+	// 	$x = 12;
+	// 	echo "time taken for code is .$x";
+	//  }
+	//  func();
+	//  echo "time taken for code is .$x";
+	 ?>
+	 <!-- --------------------------------------------------------------- -->
+	<?php
+	// $x=5;
+	// $y=8;
+	// function funn(){
+	// 	global $x,$y;
+	// 	echo "Time taken for code is .".$x*$y;
+	// }
+	// funn();
+	// echo "Time taken for code is .".$x*$y;
+	?>
+	<!-- ----------------------------------------------------------------------- -->
+	<?php
+	// $x=5;
+	// $y=8;
+	// function funn(){
+	// 	global $x,$y;
+	// 	echo $z = $x*$y;
+	// }
+	// funn();
+	// echo "Time taken for code is .".$z = $x*$y;
+	?>
+	<!-- ---------------------------------------------------------------------------- -->
+	 <?php
+	//  $x = 5;
+	//  $y = 8;
+	//  function funn(){
+	// 	static $x = 0;
+	// 	echo $x;
+	// 	$x++;
+	//  }
+	//  funn();
+	//  funn();
+	//  funn();
+	//  echo "X represrnt the numbers .$x";
+	 ?>
+
+	 <!-- ------------------------------------------------------------------------------- -->
+	 <?php 
+	//   $text = "Project";
+	//   $name = "CRM";
+	//   echo "we need the $text to be done before $name";
+	//   echo"<br>";
+	//   echo 'we need the '.$text. ' to be done before '.$name;
+	?>
+	  <!-- ------------------------------------------------------------------------------------ -->
+	  <?php 
+	//   $name = 'Linus';
+	//   echo '<h1>Hello '.$name.' </h1>';
+	//   echo "<h1>Hello $name</h1>";
+	  ?>
+	  <!-- ----------------------------------------------------------------------------------------- -->
+	   <?php
+	//    class Car{
+	// 	public $color;
+	// 	public $model;
+	// 	public function __construct($color, $model){
+	// 		$this->color = $color;
+	// 		$this ->model = $model;
+	// 	}
+
+	// 	public function message(){
+	// 		return "My car is a ".$this->color ." ".$this->model ." !";
+	// 	}
+	//    }
+	//    $myCar = new Car("red","Volvo");
+	//    var_dump($myCar);
+	   ?>
+	   <!-- -------------------------------------------------------------------------------------------- -->
+		<?php
+		// echo strlen("HELLOEVETYONE");
+		?>
+		<!-- --------------------------------------------------------------------------------------- -->
+		 <?php
+		//  echo str_word_count("Hello my project is working good.");
+		?>
+		<!-- --------------------------------------------------------------------------------------------- -->
+		<?php
+		//   $x = " Hello world";
+		//   echo trim($x);
+		?>
+		<!-- --------------------------------------------------------------------------------------- -->
+		<?php
+		// $x = "Please give some books to him.";
+		// $y = explode(" ",$x);
+		// print_r($y);
+		?>
+		<!-- -------------------------------------------------------------------------------------- -->
+		<?php
+		// $x = "Hello";
+		// $y = "World";
+		// $z = $x.$y;
+		// $w = "$x$y";
+		// echo $z;
+		// echo $w;
+		?>
+		<!-- ----------------------------------------------------------------------------------------------- -->
+		 <?php
+		  $x = "Hello World!";
+		  $y = substr($x,-5,3);
+		  echo($y);
+		 ?>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
